ec1ad35e21cae14d14a8f36bb0080596fd57ecb9
varkhipenka <shadowmoses.cfg@gmail.com>
2019-10-12 17:02:02 +0300

    add api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Api {
+    
+    _apiBase = 'https://rickandmortyapi.com/api/';
+    
+    async getResponse(url){
+        const result = await fetch(`${this._apiBase}${url}`);
+        
+        if(!result.ok){
+            throw new Error(`Fail ${url} with ${result.status}`)
+        }
+        
+        return await result.json();
+    }
+    
+    async getAllCharacters(){
+        const result = await this.getResponse(`/character`);
+        return result.results;
+    }
+    
+    getCharacter(id){
+        return this.getResponse(`/character/${id}`);
+    }
+    
+    async getAllLocations(){
+        const result = await this.getResponse(`/location`);
+        return result.results;
+    }
+    
+    getLocation(id){
+        return this.getResponse(`/location/${id}`);
+    }
+    
+    async getAllEpizodes(){
+        const result = await this.getResponse(`/episode`);
+        return result.results;
+    }
+    
+    getEpizode(id){
+        return this.getResponse(`/episode/${id}`);
+    }
+}
