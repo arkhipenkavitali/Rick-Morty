35aadb16c371528466e04d6fe9d1877537cc2591
varkhipenka <shadowmoses.cfg@gmail.com>
2019-10-21 16:38:56 +0300

    add loader

--- src/components/RandomSection/RandomSection.js
+++ src/components/RandomSection/RandomSection.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import Api from '../../services/Api';
 
 import './RandomSection.css';
+import Spinner from "../Spinner/Spinner";
 
 export default class RandomSection extends Component {
 
   api = new Api();
 
   state = {
-    planet: {}
+    randomItem: {},
+    loading: true
   };
 
   constructor(props){
@@ -18,43 +20,61 @@ export default class RandomSection extends Component {
 
   getInfo(){
     const random = Math.floor(Math.random() * 20 + 1);
-    return this.api.getCharacter(random).then((planet)=>{
-      this.setState({planet});
+    return this.api.getCharacter(random).then((randomItem)=>{
+      this.setState({
+          randomItem,
+          loading: false
+      });
     });
   }
 
   render() {
-    const {planet: {image, name, gender, species, status, created}} = this.state;
+    const {randomItem, loading} = this.state;
+    
+    const loader = loading ? <Spinner /> : null;
+    const content = !loading ? <RandomView item={randomItem} /> : null;
 
     return (
         <div className="random">
-          <div className="random__image">
-            <p>{name}</p>
-            <img src={image} alt={name}/>
-          </div>
-          <div className="random__info">
-            <p className="random__item">
-              <span>Gender:</span>
-              <span>{gender}</span>
-            </p>
-            <p className="random__item">
-              <span>Species:</span>
-              <span>{species}</span>
-            </p>
-            <p className="random__item">
-              <span>Status:</span>
-              <span>{status}</span>
-            </p>
-            <p className="random__item">
-              <span>Created:</span>
-              <span>{created}</span>
-            </p>
-          </div>
-          <div className="random__image">
-            <p>{name}</p>
-            <img src={image} alt={name}/>
-          </div>
+            {loader}
+            {content}
         </div>
     )
   }
+};
+
+const RandomView = ({item}) => {
+    
+    const {image, name, gender, species, status, created} = item;
+    
+    return (
+        <React.Fragment>
+            <div className="random__image">
+                <p>{name}</p>
+                <img src={image} alt={name}/>
+            </div>
+            <div className="random__info">
+                <p className="random__item">
+                    <span>Gender:</span>
+                    <span>{gender}</span>
+                </p>
+                <p className="random__item">
+                    <span>Species:</span>
+                    <span>{species}</span>
+                </p>
+                <p className="random__item">
+                    <span>Status:</span>
+                    <span>{status}</span>
+                </p>
+                <p className="random__item">
+                    <span>Created:</span>
+                    <span>{created}</span>
+                </p>
+            </div>
+            <div className="random__image">
+                <p>{name}</p>
+                <img src={image} alt={name}/>
+            </div>
+        </React.Fragment>
+    )
 };
---
+++ src/components/Spinner/index.js
@@ -0,0 +1,3 @@
+import Spinner from './RandomSection';
+
+export default Spinner;
